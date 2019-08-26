53029cac79e612f7d90ced9df355440aa46c9629
BURGER KNIGHT <53789314+Hilazo@users.noreply.github.com>
2019-08-26 12:50:09 -0400

    Add files via upload

---
+++ wander room.py
@@ -0,0 +1,70 @@
+from itertools import cycle
+
+rooms = [1,2]
+while wandering = True
+
+
+def wander:
+    if rooms > 0:
+        (wandering = True
+        else wandering = False)
+
+
+def wandering(rooms):
+    next(rooms), door
+
+
+print("You enter a dark room with two doors.  Do you go through door #1 or door #2?")
+door = input("> ")
+
+
+if door == "1":
+    rooms.remove(1)
+    print("There's a giant bear here eating a cheees cake.  What do you do?")
+    print("1. Take the cake")
+    print("2. Scream at the bear.")
+
+    bear = input("> ")
+
+    if bear == "1":
+        print("The bear eats your face off. OH NO!")
+    elif bear == "2":
+        print("The bear eats your legs off. UH OH!")
+    else:
+        print("Well, doing %s is probably better.  Bear runs away." %bear)
+
+elif door == "2":
+    rooms.remove(2)
+    print("You stare into the endless abyss at Cthulu's retina.")
+    print("1. Bloobs")
+    print("2. Scissors harikiri bottle rocket")
+    print("3. At last I truly see!  Grant them eyes on their brains!")
+
+    insanity = input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello.  Great job!")
+    else:
+        print("The insanity picks away at you.  You can feel it creeping inside.  Wow cool!")
+
+else:
+    print("You stumble around and fall on a knife and die.  Try again!")
+
+print("This room is empty now.  How will you proceed?")
+print("1: Do you make this your room? 2: Or check the other door?")
+room = input(">")
+if room == "1":
+    print("You begin to think about what you can do with this room")
+
+else:
+    wander
+
+
+###else:
+#   while running:
+#       for element , next_element in rooms
+#def pairwise(seq):
+#   a,b = tee(seq)
+#   next(b)
+#   return izip(a,b)
+#   for elem, next_elem in pairwise(cycle9li)):###
